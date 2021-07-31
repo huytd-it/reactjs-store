46a048fb85169edc98898ff1240b12c17bd7c2e5
huytd-it <yukihuy9999@gmail.com>
2021-07-31 23:08:19 +0700

    add files for github pages

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: '',
+      searchField: "",
     };
     // this.handleChange = this.handleChange.bind(this);
   }
-
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -21,20 +20,23 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
-  handleChange = e => {
-    this.setState({searchField: e.target.value })
-  }
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
-   
-    const { monsters, searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
-     
       <div className="App">
-        <SearchBox placeholder="search monster" handleChange={this.handleChange}></SearchBox>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.onSearchChange}
+        ></SearchBox>
         <CardList monsters={filteredMonsters}></CardList>
-       
       </div>
     );
   }
