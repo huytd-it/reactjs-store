67867bfdb055ca07826529da8d9bb40d25307b03
huytd-it <yukihuy9999@gmail.com>
2021-08-22 21:14:46 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
+import {Route} from 'react-router-dom';
 
+import HomePage from "./components/pages/homepage/homepage.component";
+import ShopPage from "./components/pages/shop/shop.component.jsx";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage}/>
     </div>
   );
 }
